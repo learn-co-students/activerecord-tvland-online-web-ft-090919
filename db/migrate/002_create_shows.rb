945fbd389b46320da1c5af34c53802b8b2256d34
Richard Pascarelli <Rpavesi84@gmail.com>
2019-10-17 00:41:04 -0400

    Done.

--- db/migrate/002_create_shows.rb
+++ db/migrate/002_create_shows.rb
@@ -1,4 +1,4 @@
-# DO NOT EDIT!
+\# DO NOT EDIT!
 # If you need to add things to shows, you need to create a new migration file
 # and additively change the schema. Always add new migrations to change the db.
 class CreateShows < ActiveRecord::Migration[5.1]
