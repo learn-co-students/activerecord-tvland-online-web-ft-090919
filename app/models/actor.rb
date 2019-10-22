cacb28c8f0ab73e9162a2dc0a516f2b955de6c92
Nightrose2016 <lrbarrer@gmail.com>
2019-10-22 16:38:30 -0400

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,17 @@
 class Actor < ActiveRecord::Base
+    has_many :characters
+    has_many :shows, through: :characters
   
-end
+    def full_name
+        "#{self.first_name} #{self.last_name}"
+    end
+  
+    def list_roles
+        roles = []
+        characters = self.characters.map {|c| c.name}
+        shows = self.shows.map {|s| s.name}
+        array = characters.concat(shows)
+        roles << array.join(" - ")
+        roles
+    end
+  end
